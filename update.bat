ee12da81196795e58441b722a1127f52f0cee77f
haniokasai♡ <htek@haniokasai.com>
2016-09-10 17:30:15 +0900

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,6 +1,2 @@
-del ClearSky.phar
-utils\bin\wget.exe http://jenkins.clearskyteam.org/job/ClearSky-php7/lastSuccessfulBuild/artifact/releases/*zip*/releases.zip
-utils\bin\unzip.exe releases.zip
-utils\bin\cp.exe releases\*.phar ClearSky.phar
-del /Q releases 
-del /Q releases.zip
+del ClearSky-PHP7.phar
+utils\bin\wget.exe http://jenkins.haniokasai.com/job/ClearSky-PHP7/lastSuccessfulBuild/artifact/artifacts/ClearSky-PHP7.phar
